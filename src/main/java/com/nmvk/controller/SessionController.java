d5440a63fb10dd899ff45833e5c267a042b9a39c
Raghav <rmuddur@gmail.com>
2017-03-14 21:45:34 -0400

    Login page and index page template
    Added login page and index page template. Updated sessions controller to
    temporarily access the views.

--- src/main/java/com/nmvk/controller/SessionController.java
+++ src/main/java/com/nmvk/controller/SessionController.java
@@ -1,10 +1,24 @@
 package com.nmvk.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * This controller is used for authentication.
  * 
  * @author Raghav
  */
+@Controller
 public class SessionController {
 
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
 }

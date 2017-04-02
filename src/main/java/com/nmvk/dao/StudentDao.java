935b623a964d910bb8dafff64b85058e980c4c65
Sunil Narasimhamurthy <snarasi5@ncsu.edu>
2017-04-01 22:21:45 -0400

    Student update as per DB tables. Added loging for student

--- src/main/java/com/nmvk/dao/StudentDao.java
+++ src/main/java/com/nmvk/dao/StudentDao.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.nmvk.domain.AppUser;
 import com.nmvk.domain.Student;
 
 public interface StudentDao extends CrudRepository<Student, Long>{
@@ -11,4 +12,8 @@ public interface StudentDao extends CrudRepository<Student, Long>{
 	@Query(value = "INSERT INTO STUDENT(STUDENT_ID, F_NAME, LNAME, DOB, LVL, RESIDENCY, BILL) VALUES (?1, ?2,?3, ?4, ?5,?6,?7)", nativeQuery = true)
 	void insert(Integer studentId, String firstName, String lastName, String dateOfBirth, Integer level, Integer residency
 			,Integer amount);
+	
+	@Query(value = "SELECT * FROM STUDENT WHERE STUDENT_ID = 1", nativeQuery = true)
+	public Student getStudentInfo();
+	
 }
